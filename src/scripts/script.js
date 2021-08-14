6345b49b22d3716411f371966e7a191d10283b18
Renan Paixão <renanlemos21@gmail.com>
2021-08-14 10:27:40 -0300

    feat(menu-hamburger): criação da funcionalidade do menu hamburguer

---
+++ src/scripts/script.js
@@ -0,0 +1,11 @@
+const hamburguer = document.querySelector('.nav__hamburguer');
+const menu = document.querySelector('.menu');
+
+function toggleMenu() {
+  menu.style.display != 'none'
+    ? (menu.style.display = 'none')
+    : (menu.style.display = 'flex');
+}
+hamburguer.addEventListener('click', () => {
+  toggleMenu();
+});
