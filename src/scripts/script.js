60a2721f0014256e452b40648f01c49a2cadc3a2
Renan Paixão <renanlemos21@gmail.com>
2021-08-15 23:13:19 -0300

    feat(form): adicionada mecânica de mudança da cor do label

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,7 @@
 const hamburguer = document.querySelector('.nav__hamburguer');
 const menu = document.querySelector('.menu');
+const labels = Array.from(document.getElementsByClassName('form__label'));
+const inputs = Array.from(document.getElementsByClassName('form__input'));
 
 function toggleMenu() {
   menu.style.display != 'none'
@@ -8,6 +10,18 @@ function toggleMenu() {
 }
 hamburguer.addEventListener('click', () => {
   toggleMenu();
+});
+
+inputs.forEach((element, index) => {
+  element.addEventListener('focusin', () => {
+    labels[index].style.color = 'var(--form-blue)';
+  });
+});
+
+inputs.forEach((element, index) => {
+  element.addEventListener('focusout', () => {
+    labels[index].style.color = 'var(--gray-article)';
+  });
 });
 
 toggleMenu();
